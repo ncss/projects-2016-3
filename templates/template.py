f3dd8498c9826c9f51a9bb474e7e99f2ce597d8e
Kosta Dunn <kostacoffee@gmail.com>
2016-01-10 09:30:20 +1100

    removing 'test' files form /engine and /templates directories

--- templates/template.py
+++
@@ -1,9 +0,0 @@
-class ParseError(Exception):
-    def __init__(self):
-        pass
-
-def _parse_template(template, upto, parent):
-    # TODO
-        
-def parse_template(template):
-    return _parse_template(template, 0 None)
