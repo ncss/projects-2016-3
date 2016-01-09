f5102cd9486094c61865ce304fbb5269001d309b
AiryShift <religionofaperturelabs@gmail.com>
2016-01-09 15:49:51 +1100

    Create template.py
    Add boilerplate todo code

---
+++ templates/template.py
@@ -0,0 +1,9 @@
+class ParseError(Exception):
+    def __init__(self):
+        pass
+
+def _parse_template(template, upto, parent):
+    # TODO
+        
+def parse_template(template):
+    return _parse_template(template, 0 None)
