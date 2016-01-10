bf8c9bfbdf96e8be922fd2ef5a5adda22ca50e39
Mark Sonnenschein <marksonn5@gmail.com>
2016-01-10 17:50:52 +1100

    Updates to models

--- models/Post.py
+++ models/Post.py
@@ -1,9 +1,10 @@
 class Post:
-	def __init__ (self, title, message, author, status):
-		self.title = title
-		self.message = message
-		self.author = author
-		self.status = status
+	def __init__ (self, post_id, message, author_id, status, timestamp):
+		self._post_id = post_id
+		self._message = message
+		self._author_id = author_id
+		self._status = status
+		self._timestamp = timestamp
 
 	def __str__(self):
 		return 'Obect for post {} by {}'.format(self.title, self.author)
@@ -11,6 +12,31 @@ class Post:
 	'''
 	Everything after this point needs to be updated in the database too!
 	'''
+
+#********************************************************************************
+#********************************************************************************
+#Return fields
+#********************************************************************************
+#********************************************************************************
+	def get_post_id(self):
+		return self._post_id
+	
+	def get_message(self):
+		return self._message
+
+	def get_author_id(self):
+		return self._author_id
+
+	def get_status(self):
+		return self._status
+
+	def get_timestamp(self):
+		return self._timestamp
+
+
+	def get_recent_10(self):
+		pass
+		#return select(<recent 10 posts>)
 
 	#def newPost(self, title, message, author, status):
 	#def Post(self, title, message, author, status):
