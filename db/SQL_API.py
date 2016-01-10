10214d52cc71c2687ef61043892f9a90951e63cb
Mark Sonnenschein <marksonn5@gmail.com>
2016-01-10 14:44:11 +1100

    Working DB

--- db/SQL_API.py
+++ db/SQL_API.py
@@ -6,6 +6,16 @@ cur = connect.cursor()
 #***************************************
 
 
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#DELETE THIS BEFORE TESTING AND PRODUCTION
+
+cur.execute('drop table if exists skills;')
+cur.execute('drop table if exists user_skills;')
+cur.execute('drop table if exists user;')
+cur.execute('drop table if exists post;')
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 cur.execute('''
 
@@ -15,8 +25,7 @@ cur.execute('''
 		specialisation text not null,
 		rank integer not null,
 
-		primary key (skill_id),
-		foreign key (skill_id) references user_skills (skill_id)
+		primary key (skill_id)
 	);
 
 ''')
@@ -36,40 +45,46 @@ cur.execute('''
 cur.execute('''
 
 	create table user (
+		user_id integer not null,
 	    email text not null,
 	    fname text not null,
 	    lname text not null,
-	    DOB text,
-	    location text,
-	    gender text,
-	    photo blob,
+	    day integer not null,
+	    month integer not null,
+	    year integer not null,
+	    location text not null,
+	    gender text not null,
+	    photo blob null,
 	    password text not null,
-	    phone text,
-	    other text,
-	    primary key(user_id) not null
-);
+	    phone text null,
+	    other text null,
+	    primary key(user_id)
+	);
 
 
 ''')
 
 cur.execute('''
 
-	create table user_skills (
-		user_id integer not null,
-		skill_id integer not null,
+	create table post (
+		post_id integer not null,
+	    message text not null,
+	    author_id integer not null,
+	    status integer not null,
+	    timestamp text not null,
 
-		foreign key (user_id) references user (user_id)
-		foreign key (skill_id) references skills (skill_id)
+	    primary key (post_id)
+	    foreign key (author_id) references user (user_id)
 	);
 
 ''')
 
 
+#insert into user values ('sofia123@mail.com', 'Sofia', 'HeLlo', '20/12/98', '40.7127, -74.0059', 'F', null, '56_ol%' 
+
+
 # for row in cur:
 # 	cur.fetchone()
-
-
-
 
 #***************************************
 #Cleanup
