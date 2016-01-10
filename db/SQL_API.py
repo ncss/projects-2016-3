e9250d7f784338ba3df43c527495a4fd3b93f624
Mark Sonnenschein <marksonn5@gmail.com>
2016-01-10 11:56:17 +1100

    New tables

--- db/SQL_API.py
+++ db/SQL_API.py
@@ -1,15 +1,29 @@
 import sqlite3
 #***************************************
 #Setup
-connect = sqlite.connect('database.db')
+connect = sqlite3.connect('database.db')
 cur = connect.cursor()
 #***************************************
 
 
 
-cur.execute('<SQL CODE>')
-for row in cur:
-	cur.fetchone()
+cur.execute('''
+
+	create table skills (
+		skill_id integer not null,
+		category text not null,
+		specialisation text not null,
+		rank integer not null,
+
+		primary key (skill_id),
+		foreign key (skill_id) references user_skills (skill_id)
+	);
+
+''')
+
+
+# for row in cur:
+# 	cur.fetchone()
 
 
 
