fd5142f9501f8ae90b767835a2fec0acbee93494
Mark Sonnenschein <marksonn5@gmail.com>
2016-01-12 05:08:24 +1100

    added update to dbfunctions

--- models/dbfunctions.py
+++ models/dbfunctions.py
@@ -31,11 +31,11 @@ def insert(table, columnvaluedict):
     connect.close()
     return inserted_id
 
-def update(table, feild, value):
+def update(table, field, value, whereClause):
     connect = sqlite3.connect('db/database.db')
     cur = connect.cursor()
-    cur.execute('''update %s set %s = %s''' % (table, field, value))
+    cur.execute('''update %s set %s = '%s' where %s;''' % (table, field, value, whereClause))
     results = cur.fetchall()
+    connect.commit()
     cur.close()
     connect.close()
-    return results
